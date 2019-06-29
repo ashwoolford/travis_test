3f4f6c340f6b63459f57b51d21ba35e5930ce6aa
Ashraf Hossain <ashrafhossain@Ashrafs-MacBook-Pro.local>
2019-06-29 16:38:35 +0600

    test commit

---
+++ pkg/calculations.py
@@ -0,0 +1,13 @@
+class CalculatioinsTest:
+
+	def __init__(self):
+		pass
+
+	def add(self, a ,b):
+		return a+b
+
+	def mul(self, a, b):
+		return a * b
+
+	def sub(self, a, b):
+		return abs(a - b)
---
+++ tests/test_calculations.py
@@ -0,0 +1,32 @@
+import unittest 
+from travis_test.pkg.calculations import CalculatioinsTest 
+
+
+class TestStemmer(unittest.TestCase):
+
+
+	def setUp(self):
+		self.t = CalculatioinsTest()
+		print('Setup')
+
+	def tearDown(self):	
+		print('Tear Down\n')
+
+	def test_add(self):
+		self.assertEqual(self.t.add(2,3),5)
+		self.assertEqual(self.t.add(2,5),7)
+		self.assertEqual(self.t.add(4,2),6)
+
+	def test_sub(self):
+		self.assertEqual(self.t.sub(3,2),1)
+		self.assertEqual(self.t.sub(5,1),4)
+		self.assertEqual(self.t.sub(4,2),2)
+
+	def test_mul(self):
+		self.assertEqual(self.t.mul(2,3),6)
+		self.assertEqual(self.t.mul(2,5),10)
+		self.assertEqual(self.t.mul(4,2),8)		
+
+
+if __name__ == '__main__':
+	unittest.main()		
